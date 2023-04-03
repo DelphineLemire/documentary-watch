15f8b9fbee5f0315a958024ba09dabeea6314482
DelphineLemire <delphine.lemire@gmail.com>
2023-04-03 14:50:44 +0200

    FIX/ replace materialize by bulma by CDN

--- documentary_watch/views.py
+++ documentary_watch/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from various.models import Theme
+
 
 def index(request):
-
+    themes = Theme.objects.all()
     return render(
         request,
         "documentary_watch/index.html",
+        {'themes': themes}
     )
